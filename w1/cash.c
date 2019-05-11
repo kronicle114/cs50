8cea8713959bb82a6070f339bcbb88bab25e52b2
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-05-11 17:55:08 -0400

    brute force solution to cash

--- w1/cash.c
+++ w1/cash.c
@@ -4,26 +4,50 @@
 
 // 1. get prompt, 2. make prompt non-negative 3. convert answer to int, 4. do the math, 5. return this number (print)
 int main(void){
-    float q;
+    float total;
     do {
-        q = get_float("Change owed: \n");
-    } while ( q < 0);
+        total = get_float("Change owed: \n");
+    } while ( total < 0);
     
-    printf("Please return %f \n", q);
+    printf("Please return %f in change \n", total);
     int counter = 0;
-    float totalcoins = q - floorf(q);
-    int dollars = floorf(q);
+    float totalcoins = total - floorf(total);
+    
+    //DOLLARS
+    int dollars = floorf(total);
     counter = dollars *4;
-    printf("counter dollars %i \n", counter);
+    printf("%i quarters in %i dollars \n", counter, dollars);
+    
+    //QUARTERS
     int qct = floorf(totalcoins/0.25);
-    printf("number of quarters needed %i\n", qct);
+    printf("%i quarters needed in coins\n", qct);
     totalcoins = totalcoins - (0.25 * qct);
     counter = counter + qct;
-    printf("coins after first iteration: %f \n", totalcoins);
-    printf("new counter total %i \n", counter);
-    //     float dimes = (quarters/0.10);
-    //     float nickels = (dimes/0.05);
-    //     float pennies = (nickels/0.01);
-    printf("dollars: %i \n", dollars);
-    printf("coins: %f \n", totalcoins);
+    //     printf("%f new balance after dollars & quarters taken out \n", totalcoins);
+    //     printf("%i new counter total \n", counter);
+    
+    //DIMES
+    int dimes = floorf(totalcoins/0.10);
+    printf("%i dimes needed in coins\n", dimes);
+    totalcoins = totalcoins - (0.10 * dimes);
+    counter = counter + dimes;
+    //     printf("%f new balance after dollars, quarters, dimes taken out \n", totalcoins);
+    //     printf("%i new counter total \n", counter);
+    
+    //NICKELES
+    int nickles = floorf(totalcoins/0.05);
+    printf("%i nickles needed in coins\n", nickles);
+    totalcoins = totalcoins - (0.05 * nickles);
+    counter = counter + nickles;
+    printf("%f new balance after dollars, quarters, dimes taken out \n", totalcoins);
+    printf("%i new counter total \n", counter);
+    
+    //PENNIES
+    int pennies = floorf(totalcoins/0.01);
+    printf("%i pennies needed in coins\n", pennies);
+    totalcoins = totalcoins - (0.01 * pennies);
+    counter = counter + pennies;
+    printf("%f new balance after all is taken out \n", totalcoins);
+    printf("%i new counter total \n", counter);
+    
 }
