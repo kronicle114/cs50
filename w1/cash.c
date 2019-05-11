3734a72ed280a17242cad92ee93400fb5a9ef179
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-05-11 18:42:41 -0400

    added if else to prevent negative values

--- w1/cash.c
+++ w1/cash.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <math.h>
 
-// 1. get prompt, 2. make prompt non-negative 3. convert answer to int, 4. do the math, 5. return this number (print)
+// 1. get prompt, 2. make prompt non-negative 3. convert answer to int, 4. do the math, 5. return or print least amount of change needed
 int main(void){
     float total;
     do {
@@ -13,41 +13,39 @@ int main(void){
     int counter = 0;
     float totalcoins = total - floorf(total);
     
-    //DOLLARS
-    int dollars = floorf(total);
-    counter = dollars *4;
-    printf("%i quarters in %i dollars \n", counter, dollars);
-    
-    //QUARTERS
-    int qct = floorf(totalcoins/0.25);
-    printf("%i quarters needed in coins\n", qct);
-    totalcoins = totalcoins - (0.25 * qct);
-    counter = counter + qct;
-    //     printf("%f new balance after dollars & quarters taken out \n", totalcoins);
-    //     printf("%i new counter total \n", counter);
-    
-    //DIMES
-    int dimes = floorf(totalcoins/0.10);
-    printf("%i dimes needed in coins\n", dimes);
-    totalcoins = totalcoins - (0.10 * dimes);
-    counter = counter + dimes;
-    //     printf("%f new balance after dollars, quarters, dimes taken out \n", totalcoins);
-    //     printf("%i new counter total \n", counter);
-    
-    //NICKELES
-    int nickles = floorf(totalcoins/0.05);
-    printf("%i nickles needed in coins\n", nickles);
-    totalcoins = totalcoins - (0.05 * nickles);
-    counter = counter + nickles;
-//    printf("%f new balance after dollars, quarters, dimes taken out \n", totalcoins);
-//    printf("%i new counter total \n", counter);
-    
-    //PENNIES
-    int pennies = ceilf(totalcoins/0.01);
-    printf("%i pennies needed in coins\n", pennies);
-    totalcoins = totalcoins - (0.01 * pennies);
-    counter = counter + pennies;
-//    printf("%f new balance after all is taken out \n", totalcoins);
-//    printf("%i new counter total \n", counter);
-    
+    if(counter != 0 || total != 0){
+        //DOLLARS
+        int dollars = floorf(total);
+        counter = dollars *4;
+        printf("%i quarters in %i dollars \n", counter,         dollars);
+        //QUARTERS
+        int qct = floorf(totalcoins/0.25);
+        printf("%i quarters needed in coins\n", qct);
+        totalcoins = totalcoins - (0.25 * qct);
+        counter = counter + qct;
+        
+        //DIMES
+        int dimes = floorf(totalcoins/0.10);
+        printf("%i dimes needed in coins\n", dimes);
+        totalcoins = totalcoins - (0.10 * dimes);
+        counter = counter + dimes;
+        
+        //NICKELES
+        int nickles = floorf(totalcoins/0.05);
+        printf("%i nickles needed in coins\n", nickles);
+        totalcoins = totalcoins - (0.05 * nickles);
+        counter = counter + nickles;
+        
+        //PENNIES
+        int pennies = ceilf(totalcoins/0.01);
+        printf("%i pennies needed in coins\n", pennies);
+        totalcoins = totalcoins - (0.01 * pennies);
+        counter = counter + pennies;
+        
+        printf("change given: %i \n", counter);
+        return counter;
+    }
+    else {
+        printf("zero!\n");
+    }
 }
